dfa915553d167665088cff1044c76ea291af5692
Your Name <you@example.com>
2015-04-15 19:34:50 +0800

    4-15

---
+++ tetris/zone.cpp
@@ -0,0 +1,7 @@
+#include "zone.h"
+#
+
+void CGrid::Paint()
+{
+  
+};
---
+++ tetris/zone.h
@@ -0,0 +1,44 @@
+virtual class CZone
+{
+public:
+  void Paint();
+private:
+  int m_width;
+  int m_height;
+};
+
+struct SCoor
+{
+  int x, y;
+};
+
+class CLeftZone : private CZone
+{
+};
+
+class CRightZone : private CZone
+{
+};
+
+class CBlock
+{
+public:
+  
+private:
+  int m_length;
+  SCoor m_centerXY;
+};
+
+class CGrid : private CZone
+{
+private:
+  bool m_occupied;
+  int m_left, m_bottom;
+  int m_colorR, m_colorG, m_colorB;
+};
+
+class CMiddleZone : private CZone
+{
+public:
+  friend CBlock;
+};
